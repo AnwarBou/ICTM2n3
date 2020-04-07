f4fce52145cc733c076758fc03c775259cd73eca
pekka26 <55087185+pekka26@users.noreply.github.com>
2020-04-07 11:21:58 +0200

    extra !

--- ICTM2n3/Main.java
+++ ICTM2n3/Main.java
@@ -2,6 +2,6 @@ package ICTM2n3.Main.java;
 
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world!!");
     }
 }
